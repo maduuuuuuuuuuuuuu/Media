0ff678ce166b1d8d9da1e6cb110aa7ee9af2e322
madu <maduamaral2006@gmail.com>
2023-06-29 15:46:28 -0300

    exercicio resolvido

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿double numeroum;
 double numerodois;
 double numerotres;
-Console.WriteLine("descubra sua média de 3 numeros!");
-Console.WriteLine("");
-Console.WriteLine("digite um numero");
-numeroum = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("digite outro numero");
-numerodois = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("digite mais um numero");
-numerotres = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("");
+    Console.WriteLine("descubra sua média de 3 numeros!");
+    Console.WriteLine("");
+    Console.WriteLine("digite um numero");
+        numeroum = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("digite outro numero");
+        numerodois = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("digite mais um numero");
+        numerotres = Convert.ToDouble(Console.ReadLine());
 double media;
-media = numeroum + numerodois + numerotres / 3;
-Console.WriteLine($"sua média é de {media}");
+        media = (numeroum + numerodois + numerotres) / 3;
+    Console.WriteLine($"sua média é de {media}");
+    Console.WriteLine("");
